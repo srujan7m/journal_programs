9b2b491e4b2aa9849fce7e9428c3daf11b55469f
srujan7m <srujanmmattur@gmail.com>
2025-11-11 10:58:49 +0530

    simple interest

--- jp_5.py
+++ jp_5.py
@@ -3,13 +3,13 @@ import sys
 
 if len(sys.argv) == 4:
     script_name = sys.argv[0]
-    principal = float(sys.argv[1])
+    principle = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
 else:
     
     script_name = sys.argv[0]
-    principal = 1000.0
+    principle = 1000.0
     rate = 5.0
     time = 2.0
 
