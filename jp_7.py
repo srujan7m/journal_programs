f5429375a7a2b937dbbe977cd367853774e3aed3
srujan7m <srujanmmattur@gmail.com>
2025-11-11 11:51:00 +0530

    Array input

--- jp_7.py
+++ jp_7.py
@@ -1,14 +1,8 @@
-
 scores_input = input("Enter scores separated by spaces: ")
 scores = [float(num) for num in scores_input.split()]
 
 total = sum(scores)
 average = total / len(scores)
 
-maximum = max(scores)
-minimum = min(scores)
-
 print(f"Sum of scores = {total}")
 print(f"Average score = {average:.2f}")
-print(f"Maximum score = {maximum}")
-print(f"Minimum score = {minimum}")
