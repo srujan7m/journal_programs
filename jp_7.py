d3ebc38552cb2a07aae326bf6bb094f2584c78d6
srujan7m <srujanmmattur@gmail.com>
2025-11-11 11:44:44 +0530

    modifications

--- jp_7.py
+++ jp_7.py
@@ -5,5 +5,10 @@ scores = [float(num) for num in scores_input.split()]
 total = sum(scores)
 average = total / len(scores)
 
+maximum = max(scores)
+minimum = min(scores)
+
 print(f"Sum of scores = {total}")
 print(f"Average score = {average:.2f}")
+print(f"Maximum score = {maximum}")
+print(f"Minimum score = {minimum}")
