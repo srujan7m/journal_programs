af689d7b5ea08e3c452dc0a95dc007bb701ae4e1
srujan7m <srujanmmattur@gmail.com>
2025-11-11 11:40:55 +0530

    array input

---
+++ jp_7.py
@@ -0,0 +1,9 @@
+
+scores_input = input("Enter scores separated by spaces: ")
+scores = [float(num) for num in scores_input.split()]
+
+total = sum(scores)
+average = total / len(scores)
+
+print(f"Sum of scores = {total}")
+print(f"Average score = {average:.2f}")
